fc068e611ec57d8ccff66968d7753789ea3f7b93
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-07 16:16:45 +0600

    sum of number 1 to 10

--- problem.c
+++ problem.c
@@ -2,24 +2,11 @@
 #include<stdio.h>
 
 int main() {
-  int i, n;
-  float num, smallest;
-
-  printf("Enter the total number of elements:");
-  scanf("%d", &n);
-
- printf("Enter number 1:\n");
- scanf("%f", &smallest);  //10 
-
- for(i=2; i<=n; i++){
-    printf("Enter number %d:", i);
-    scanf("%f", &num); //5
-
-    if(num< smallest){
-        smallest = num;
-    }
- }
- printf("The smallest number is:%f", smallest);
-
+  int i;
+  int sum = 0;
+  for(i = 0; i<10; i++){
+    sum = sum + i;
+  }
+  printf("%d", sum);
   return 0;
 }
